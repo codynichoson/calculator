bc4c2c754a5702b77ff167530b46ced9d97fbed2
Cody Nichoson <codynichoson@gmail.com>
2026-01-12 23:02:03 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QUrl>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQuickView view;
+    view.setSource(QUrl("main.qml"));
+    view.show();
+
+    return app.exec();
+}
